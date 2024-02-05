eb359f95c4143826a65ad324dfca913eb063656a
Muhammad Osman <139002050+MuhammadOsman381@users.noreply.github.com>
2024-02-05 11:56:52 +0500

    third commit

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
-var maths = document.getElementsByClassName('maths');
-var phy = document.getElementsByClassName('phy');
-var cs = document.getElementsByClassName('cs');
+var maths = document.querySelector('.maths');
+var phy = document.querySelector('.phy');
+var cs = document.querySelector('.cs');
 var rollno = document.querySelector('#rollno');
 var btn = document.querySelector('.btn');
 
 const subjects = [
     {
-        math:"null",
-        physics:"null",
-        cscience:"null",
+        math: "null",
+        physics: "null",
+        cscience: "null",
     },
     {
-        math:"50",
-        physics:"60",
-        cscience:"70",
+        math: "50",
+        physics: "60",
+        cscience: "70",
     },
     {
-        math:"40",
-        physics:"20",
-        cscience:"10",
+        math: "40",
+        physics: "20",
+        cscience: "10",
     },
     {
-        math:"80",
-        phyics:"90",
-        cscience:"100",
+        math: "80",
+        physics: "90",
+        cscience: "100",
     }
 ];
 
-   
-btn.addEventListener('click',function(){
- 
-        if(rollno.value === '')
-        {
-            alert('Enter the rollno');
-        }
+btn.addEventListener('click', function () {
+    if (rollno.value === '') 
+    {
+        alert('Enter the rollno');
+    } 
+    else {
 
-        for(let i=0; i<=subjects.length; i++)
-        {
-            if(rollno.value === i)
+        let rollNumber = parseInt(rollno.value);
+        
+        for (let i = 0; i < subjects.length; i++) {
+            if (rollNumber === i)
             {
                 maths.innerHTML = subjects[i].math;
                 phy.innerHTML = subjects[i].physics;
                 cs.innerHTML = subjects[i].cscience;
+                break;
             }
         }
-})
+        alert('Roll number not found');
+    }
+});
